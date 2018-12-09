7d978c2c759606679c2655cbd5abeee42142c568
szczepan19niz <30689@student.pwsztar.edu.pl>
2018-12-09 12:47:52 +0100

    added some improvements

--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -4,12 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { MapComponent } from './components/map/map.component';
+import { QuizComponent } from './components/quiz/quiz.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavBarComponent
+    NavBarComponent,
+    ContactComponent,
+    MapComponent,
+    QuizComponent
   ],
   imports: [
     BrowserModule
